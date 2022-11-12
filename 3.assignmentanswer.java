a68a4d051548db0688ae629e672a900390820166
jastiyoginagasairam <111644440+jastiyoginagasairam@users.noreply.github.com>
2022-11-12 09:36:42 +0530

    Update 3.assignmentanswer.java

--- 3.assignmentanswer.java
+++ 3.assignmentanswer.java
@@ -7,10 +7,11 @@ public class abcd  {
      System.out.print("Input a number: ");
      int n = in.nextInt();
      int k =23;
-     if(n>=10 && n<=150)
-     if(n%k==0)
+     if(n>=10 && n<=150 && n%k==0)
      System.out.println("It is a divisible by 23");    
-     if(n%k!=0)
+     if(n%k!=0 && n>=10 && n<=150)
      System.out.println("not divisible by 23");
+     if(n<=10 || n>=150)
+     System.out.println("input number must>10 and <150");
      }  
 }
