1e96d0c05b565ebfa861719c597a5edc93aa8198
jastiyoginagasairam <111644440+jastiyoginagasairam@users.noreply.github.com>
2022-11-12 09:27:45 +0530

    Create 3.assignmentanswer.java

---
+++ 3.assignmentanswer.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class abcd  {
+
+    public static void main(String args[])
+    {
+	 Scanner in = new Scanner(System.in);
+     System.out.print("Input a number: ");
+     int n = in.nextInt();
+     int k =23;
+     if(n>=10 && n<=150)
+     if(n%k==0)
+     System.out.println("It is a divisible by 23");    
+     if(n%k!=0)
+     System.out.println("not divisible by 23");
+     }  
+}
